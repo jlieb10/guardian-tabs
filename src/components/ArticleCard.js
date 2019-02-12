ac8610db865243e4d5f7b8d7b176a6a5e74c4f18
Joshua Lieberman <joshuathomaslieberman@gmail.com>
2019-02-12 20:54:44 +0000

    [MISC] added fetch polyfill and node-sass package, added my preferred structured folder system to the repo

---
+++ src/components/ArticleCard.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+class ArticleCard extends PureComponent {
+  render() {
+    return (
+      <div className="ArticleCard"/>
+    );
+  }
+}
+
+export default ArticleCard;
---
+++ src/components/TabComponent.js
@@ -0,0 +1,9 @@
+import React, { PureComponent } from "react";
+
+class TabComponent extends PureComponent {
+  render() {
+    return <div className="TabComponent" />;
+  }
+}
+
+export default TabComponent;
