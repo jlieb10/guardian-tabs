9f6aacc98938bde430efd4c1e64e91cc76183244
Joshua Lieberman <joshuathomaslieberman@gmail.com>
2019-02-12 21:44:12 +0000

    [STYLES] basic functional styles in place, working on preventing flash of unstyled content

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -2,10 +2,12 @@ import React from "react";
 
 const ArticleCard = props => {
   return (
-    <a className="article-card" href={props.webUrl}>
-      <span className="article-card__number">{props.index + 1}</span>
-      <h3 className="article-card__title">{props.webTitle}</h3>
-    </a>
+    <li className="article-card">
+      <a className="article-card__link" href={props.webUrl}>
+        <span className="article-card__number">{props.index + 1}.</span>
+        <h3 className="article-card__title">{props.webTitle}</h3>
+      </a>
+    </li>
   );
 };
 
